5f5c25ac65b756044c13ca5d73464d95e652f1f2
smailahmed <ahmed.smail252@gmail.com>
2021-01-08 21:24:06 +0100

    Lesson 12, 13, 14

--- stage/js/main.js
+++ stage/js/main.js
@@ -3,4 +3,8 @@ $(function () {
   $(".toggle-sidebar").on("click", function () {
     $(".content-area, .sidebar").toggleClass("no-sidebar");
   });
+  $(".toggle-submenu").on("click", function () {
+    $(this).find(".fa-angle-right").toggleClass("down");
+    $(this).next(".child-links").slideToggle(600);
+  });
 });
