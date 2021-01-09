1c69a3fd35012149bf8adfb1d0c289674609efac
smailahmed <ahmed.smail252@gmail.com>
2021-01-09 16:28:50 +0100

    Lesson 18

--- stage/js/dist/main.dev.js
+++ stage/js/dist/main.dev.js
@@ -1,16 +1,20 @@
+"use strict";
+
 $(function () {
   "use strict";
+
   $(".toggle-sidebar").on("click", function () {
     $(".content-area, .sidebar").toggleClass("no-sidebar");
   });
   $(".toggle-submenu").on("click", function () {
     $(this).find(".fa-angle-right").toggleClass("down");
     $(this).next(".child-links").slideToggle(600);
-  });
-  // Open / Close Fullscreen
+  }); // Open / Close Fullscreen
+
   $(".toggle-fullscreen").on("click", function () {
     $(this).toggleClass("full-screen");
-    if($(this).hasClass("full-screen")) {
+
+    if ($(this).hasClass("full-screen")) {
       openFullscreen();
     } else {
       closeFullscreen();
@@ -18,26 +22,32 @@ $(function () {
   });
 });
 /* Get the documentElement (<html>) to display the page in fullscreen */
+
 var elem = document.documentElement;
 /* View in fullscreen */
+
 function openFullscreen() {
   if (elem.requestFullscreen) {
     elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
     elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
     elem.msRequestFullscreen();
   }
 }
 /* Close fullscreen */
+
+
 function closeFullscreen() {
   if (document.exitFullscreen) {
     document.exitFullscreen();
-  } else if (document.webkitExitFullscreen) { /* Safari */
+  } else if (document.webkitExitFullscreen) {
+    /* Safari */
     document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) { /* IE11 */
+  } else if (document.msExitFullscreen) {
+    /* IE11 */
     document.msExitFullscreen();
   }
 }
-const myMap = new Map();
-const mySet = new Set();
--- stage/js/main.js
+++ stage/js/main.js
@@ -7,4 +7,35 @@ $(function () {
     $(this).find(".fa-angle-right").toggleClass("down");
     $(this).next(".child-links").slideToggle(600);
   });
+  // Open / Close Fullscreen
+  $(".toggle-fullscreen").on("click", function () {
+    $(this).toggleClass("full-screen");
+    if($(this).hasClass("full-screen")) {
+      openFullscreen();
+    } else {
+      closeFullscreen();
+    }
+  });
 });
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
